a0cfc082289fbfabca259a86e194b70a76874343
Michael Yang <michael.getemail@gmail.com>
2020-05-30 19:52:44 +0100

    Added TMDb attribution

--- frontend/src/containers/user/AccountDrawer.tsx
+++ frontend/src/containers/user/AccountDrawer.tsx
@@ -190,6 +190,24 @@ function AccountDrawer() {
       <div id="account-drawer-pic">
         <img src={VCRSmallLogo} height="70px" alt="VCR logo" />
       </div>
+
+      <div className="tmdb-attribution">
+        This product uses the TMDb API but is not endorsed or certified by TMDb.
+        <div className="divider"></div>
+        <a
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={
+              "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+            }
+            height="25px"
+            alt="TMDb logo"
+          />
+        </a>
+      </div>
     </div>
   );
 
